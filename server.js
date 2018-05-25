d4e69ddf5bba7c78185b1b7da824e4b8ba006b77
Laura Salcido <idlaurasalcido@gmail.com>
2018-05-25 10:33:40 -0700

    almost working

--- server.js
+++ server.js
@@ -58,19 +58,20 @@ app.get('/newsarticles', (req, res) => {
 // });
 
 // route to saved articles
+
+app.post('/saved/:id', (req, res) => {
+    Article.update({ _id: id }).then(function (r) {
+        res.json(r)
+    })
+})
+
 app.get('/savedArticles', (req, res) => {
     Article.find({ saved: true }).then(function (r) {
         res.render('savedArticles', { sarticle: r })
     }).catch(function (e) {
         res.send(e)
-    })
+    });
 });
-
-app.post('/saved/:id', (req, res) => {
-    Article.update({_id : id}).then(function (r) {
-        res.json(r)
-    })
-})
 
 app.post('/scrape', (req, res) => {
     const scrapedUrl = 'https://www.cnet.com/news/'
@@ -94,25 +95,42 @@ app.post('/scrape', (req, res) => {
             const url = r.find('a').attr('href');
             const author = r.find('.assetAuthor').text()
 
-            if (title && body && url && author && unique) {
+            if (title && body && url && author) {
                 const articleObj = new Article({
                     title: title,
                     body: body,
                     url: 'https://www.cnet.com/news' + url,
                     author: author,
                     saved: false
-                })
+                });
 
-                console.log(articleObj.body);
-
+                console.log(articleObj.title);
+                console.log(Article.title)
                 // add if statement for repeats
-                articleObj.save((err) => {
-                    if (err) throw err;
-                })
-            }
+                if (articleObj.title !== Article.title) {
+                    articleObj.save((err) => {
+                        if (err) throw err;
+                    })
+                }; 
+            };
+            
         });
     });
+    res.redirect('/newsarticles')
 });
+
+// save post
+app.put('/note/:id', (req, res) => {
+    console.log(req.body.noteText) // returns note
+    // let text = req.body.noteText
+
+    Article.update({ _id: req.params.id }, (err, data) => {
+        if (err) throw err;
+        console.log(data)
+    });
+    res.render('newsfeed')
+});
+
 
 app.listen(3000, (err) => {
     if (err) throw err;
